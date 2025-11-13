5f5516fd37f04bffae0654a2f61f955627ea486d
Manav sagar <manavsagar46@gmail.com>
2025-11-13 12:07:05 +0530

    second commit4

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import { UserProvider } from "./Context/UserContext.jsx";
+import { UserProvider } from "./Context/UserContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
